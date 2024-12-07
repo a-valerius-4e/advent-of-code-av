ed9db57589eaab7735fc233df90619d103fe053a
Alexander Valerius <zander.valle@gmail.com>
2024-12-07 19:22:21 +0100

    feat: Day 2

---
+++ src/2024/FourEnergy.AdventOfCode.TwentyFour/FourEnergy.AdventOfCode.TwentyFour.DayOne/Day02/Day2Riddle1.cs
@@ -0,0 +1,37 @@
+﻿namespace FourEnergy.AdventOfCode.Y2024.Day02;
+
+internal static class Day2Riddle1
+{
+    public static string Solve(string input)
+    {
+        var rows = input.Split("\r\n");
+        var safeReportsCount = 0;
+        foreach (var report in rows)
+        {
+            var levels = report.Split(" ").Select(l => int.Parse(l)).ToArray();
+            var direction = levels[0] < levels[1] ? Direction.Increasing : Direction.Decreasing;
+            bool isSafe = true;
+            for (var i = 0; i < levels.Length - 1; i++)
+            {
+                var localDirection = levels[i] < levels[i + 1] ? Direction.Increasing : Direction.Decreasing;
+                var distance = Math.Abs(levels[i] - levels[i + 1]);
+                if (localDirection != direction || distance == 0 || distance > 3)
+                {
+                    isSafe = false;
+                }
+            }
+            if (isSafe)
+            {
+                safeReportsCount++;
+            }
+        }
+
+        return safeReportsCount.ToString();
+    }
+}
+
+internal enum Direction
+{
+    Increasing,
+    Decreasing
+}
---
+++ src/2024/FourEnergy.AdventOfCode.TwentyFour/FourEnergy.AdventOfCode.TwentyFour.DayOne/Day02/Day2Riddle2.cs
@@ -0,0 +1,52 @@
+﻿
+namespace FourEnergy.AdventOfCode.Y2024.Day02;
+
+internal static class Day2Riddle2
+{
+    public static string Solve(string input)
+    {
+        var rows = input.Split("\r\n");
+        var safeReportsCount = 0;
+        foreach (var report in rows)
+        {
+            var levels = report.Split(" ").Select(l => int.Parse(l)).ToArray();
+            var isReportSafe = CheckReport(levels);
+            if (!isReportSafe)
+            {
+                for (var i = 0; i < levels.Length; i++)
+                {
+                    var tempReport = ((int[])levels.Clone()).ToList();
+                    tempReport.RemoveAt(i);
+                    isReportSafe = CheckReport(tempReport.ToArray());
+                    if (isReportSafe)
+                    {
+                        break;
+                    }
+                }
+            }
+            if (isReportSafe)
+            {
+
+                safeReportsCount++;
+            }
+        }
+
+        return safeReportsCount.ToString();
+    }
+
+    private static bool CheckReport(int[] levels)
+    {
+        var direction = levels[0] < levels[1] ? Direction.Increasing : Direction.Decreasing;
+        bool isSafe = true;
+        for (var i = 0; i < levels.Length - 1; i++)
+        {
+            var localDirection = levels[i] < levels[i + 1] ? Direction.Increasing : Direction.Decreasing;
+            var distance = Math.Abs(levels[i] - levels[i + 1]);
+            if (localDirection != direction || distance == 0 || distance > 3)
+            {
+                isSafe = false;
+            }
+        }
+        return isSafe;
+    }
+}
